a64031d76a6589edb2a56c7160c698b4323abbc5
DevAryanPro <24020201150@darshan.ac.in>
2025-04-30 09:51:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,11 @@
 module.exports = (req, res) => {
-  const ping = new Date();
-  ping.setHours(ping.getHours() - 3);
-  console.log(
-    `Ping at: ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
-  );
   res.json({
     message: 'YouTube API Service',
     endpoints: {
-      info: '/api/youtube/info?url=YOUTUBE_URL',
-      mp3: '/api/youtube/mp3?url=YOUTUBE_URL',
-      mp4: '/api/youtube/mp4?url=YOUTUBE_URL',
-      docs: '/api-docs'
-    }
+      info: '/info?url=YOUTUBE_URL',
+      mp3: '/mp3?url=YOUTUBE_URL',
+      mp4: '/mp4?url=YOUTUBE_URL'
+    },
+    example: '/info?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
   });
 };
